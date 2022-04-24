f0048ebaa0d81326ea303e2db40fcfd3812156ec
James Benner <james.benner@gmail.com>
2022-04-24 16:39:07 -0400

    Add user resource

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -8,11 +8,22 @@
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
 import { Set, Router, Route } from '@redwoodjs/router'
+import UsersLayout from 'src/layouts/UsersLayout'
 import NotesLayout from 'src/layouts/NotesLayout'
 
 const Routes = () => {
   return (
     <Router>
+      <Route path="/login" page={LoginPage} name="login" />
+      <Route path="/signup" page={SignupPage} name="signup" />
+      <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
+      <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
+      <Set wrap={UsersLayout}>
+        <Route path="/users/new" page={UserNewUserPage} name="newUser" />
+        <Route path="/users/{id:Int}/edit" page={UserEditUserPage} name="editUser" />
+        <Route path="/users/{id:Int}" page={UserUserPage} name="user" />
+        <Route path="/users" page={UserUsersPage} name="users" />
+      </Set>
       <Set wrap={NotesLayout}>
         <Route path="/notes/new" page={NoteNewNotePage} name="newNote" />
         <Route path="/notes/{id:Int}/edit" page={NoteEditNotePage} name="editNote" />
