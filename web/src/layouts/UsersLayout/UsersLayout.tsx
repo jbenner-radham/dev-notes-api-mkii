f018e2d1b9dfd6a0d321a0759b964876de6d43e2
James Benner <james.benner@gmail.com>
2022-05-22 14:12:55 -0400

    Add auth

--- web/src/layouts/UsersLayout/UsersLayout.tsx
+++ web/src/layouts/UsersLayout/UsersLayout.tsx
@@ -1,3 +1,4 @@
+import { useAuth } from '@redwoodjs/auth'
 import { Link, routes } from '@redwoodjs/router'
 import { Toaster } from '@redwoodjs/web/toast'
 
@@ -6,22 +7,30 @@ type UserLayoutProps = {
 }
 
 const UsersLayout = ({ children }: UserLayoutProps) => {
+  const { isAuthenticated, currentUser, logOut } = useAuth()
+
   return (
     <div className="rw-scaffold">
       <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
       <header className="rw-header">
-        <h1 className="rw-heading rw-heading-primary">
-          <Link
-            to={routes.users()}
-            className="rw-link"
-          >
-            Users
-          </Link>
-        </h1>
-        <Link
-          to={routes.newUser()}
-          className="rw-button rw-button-green"
-        >
+        <div className="flex-between">
+          <h1 className="rw-heading rw-heading-primary">
+            <Link to={routes.users()} className="rw-link">
+              Users
+            </Link>
+          </h1>
+          {isAuthenticated ? (
+            <div>
+              <span>Logged in as {currentUser.email}</span>{' '}
+              <button type="button" onClick={logOut}>
+                Logout
+              </button>
+            </div>
+          ) : (
+            <Link to={routes.login()}>Login</Link>
+          )}
+        </div>
+        <Link to={routes.newUser()} className="rw-button rw-button-green">
           <div className="rw-button-icon">+</div> New User
         </Link>
       </header>
